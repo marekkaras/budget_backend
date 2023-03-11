be92fe55f40d69388ac81b1a6c0e482162331586
ronan-f <rfegan13@gmail.com>
2023-03-10 22:01:33 -0500

    Fixed failing tests

--- budget/test_main.py
+++ budget/test_main.py
@@ -1,10 +1,11 @@
+import pytest
 from fastapi.testclient import TestClient
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from datetime import timedelta
 from budget.database.models import Base
-from budget.database.database import get_db
+from budget.database.crud import get_db
 from budget.main import app
+from budget.database import models
 
 SQLALCHEMY_DATABASE_URL = "sqlite:///./test.db"
 
@@ -12,9 +13,11 @@ engine = create_engine(
     SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
 )
 
-TestingSessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+TestingSessionLocal = sessionmaker(
+    autocommit=False, autoflush=False, bind=engine)
 
 Base.metadata.create_all(bind=engine)
+
 
 def override_get_db():
     try:
@@ -23,68 +26,126 @@ def override_get_db():
     finally:
         db.close()
 
+    user = models.User(
+        username="test_user",
+        email="test_user@example.com",
+        hashed_password="password",
+        full_name="Test User",
+        disabled=False
+    )
+
+    return {"user": user, "token": "test_token"}
+
+
 app.dependency_overrides[get_db] = override_get_db
 
 client = TestClient(app)
 
-def test_create_user():
-    response = client.post(
-        "/create_user/",
-        json={
-            "username": "test_user",
-            "email": "test_user@example.com",
-            "password": "password"
-        },
-    )
-    assert response.status_code == 200
-    assert response.json() == {
-        "username": "test_user",
-        "email": "test_user@example.com",
-        "id": 1
-    }
 
-def test_create_existing_user():
+@pytest.fixture(scope="function")
+def db():
+    session = TestingSessionLocal()
+    yield session
+    session.close()
+    Base.metadata.drop_all(bind=engine)
+    Base.metadata.create_all(bind=engine)
+
+
+def test_create_user(db):
     response = client.post(
         "/create_user/",
         json={
             "username": "test_user",
             "email": "test_user@example.com",
-            "password": "password"
+            "password": "password",
+            "full_name": "Test User"
         },
     )
+
+    assert response.status_code == 200
+    assert response.json()["username"] == "test_user"
+    assert response.json()["email"] == "test_user@example.com"
+    assert response.json()["full_name"] == "Test User"
+    assert response.json()["hashed_password"] is not None
+    assert response.json()["hashed_password"] != "password"
+
+
+def test_create_existing_user(db):
+    # Create a user
+    response = client.post(
+        "/create_user/",
+        json={
+            "username": "test_user",
+            "email": "test_user@example.com",
+            "password": "password",
+            "full_name": "Test User"
+        },
+    )
+
+    # Make sure the user was created successfully
+    assert response.status_code == 200
+
+    # Try to create the same user again
+    response = client.post(
+        "/create_user/",
+        json={
+            "username": "test_user",
+            "email": "test_user@example.com",
+            "password": "password",
+            "full_name": "Test User"
+        },
+    )
+
+    # Make sure the endpoint returns a 400 status code and an error message
     assert response.status_code == 400
     assert response.json() == {"detail": "Username already registered"}
 
+
 def test_add_budget_for_user():
     response = client.post(
-        "/add_budget_for_user/",
-        headers={"Authorization": "Bearer test_token"},
+        "/create_user/",
         json={
-            "name": "test_budget",
-            "description": "test_description",
-            "amount": 1000
+            "username": "test_user_2",
+            "email": "test_user_2@example.com",
+            "password": "password",
+            "full_name": "Test User"
         },
     )
-    assert response.status_code == 200
-    assert response.json() == {
-        "name": "test_budget",
-        "description": "test_description",
-        "amount": 1000,
-        "uuid": 1,
-        "user_id": 1,
-        "created_at": "2023-03-10T12:00:00",
-        "updated_at": "2023-03-10T12:00:00"
+
+    form_data = {
+        "username": "test_user_2",
+        "password": "password"
     }
+    token_response = client.post("/token", data=form_data)
+    access_token = token_response.json()["access_token"]
+
+    headers = {"Authorization": f"Bearer {access_token}"}
+    data = {
+        "base_ccy": "USD",
+        "month": 3,
+        "year": 2023,
+        "amount": 1000
+    }
+
+    response = client.post("/add_budget_for_user/", headers=headers, json=data)
+
+    assert response.status_code == 200
+
+    assert response.json()["base_ccy"] == "USD"
+    assert response.json()["amount"] == 1000.0
+    assert response.json()["month"] == 3
+    assert response.json()["year"] == 2023
+
 
 def test_add_budget_for_nonexistent_user():
     response = client.post(
         "/add_budget_for_user/",
         headers={"Authorization": "Bearer test_token"},
         json={
-            "name": "test_budget",
-            "description": "test_description",
+            "base_ccy": "USD",
+            "month": 3,
+            "year": 2023,
             "amount": 1000
-        },
+        }
     )
-    assert response.status_code == 400
-    assert response.json() == {"detail": "Username doesnt exist in database"}
+    assert response.status_code == 401
--- budget/users/test_auth.py
+++ budget/users/test_auth.py
@@ -1,87 +1,98 @@
-from datetime import datetime, timedelta
-
-from fastapi import HTTPException
-from sqlalchemy.orm import Session
-
-from budget.database.crud import create_user, delete_all_users, get_user_by_username
-from budget.database.database import get_db
+from budget.users.auth import (
+    verify_password, get_password_hash, get_user, authenticate_user, create_access_token)
+from datetime import timedelta
+from budget.database import schemas
+from budget.database.crud import create_user
+from budget.database.crud import get_db
 from budget.main import app
-from budget.models.user import UserCreate
-from budget.tests.utils.utils import (assert_response_success, assert_response_error, 
-                                      create_random_email, get_superuser_token_headers)
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from budget.database.models import Base
+import pytest
+
+SQLALCHEMY_DATABASE_URL = "sqlite:///./test.db"
+
+engine = create_engine(
+    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
+)
+
+TestingSessionLocal = sessionmaker(
+    autocommit=False, autoflush=False, bind=engine)
+
+Base.metadata.create_all(bind=engine)
+
+
+def override_get_db():
+    try:
+        db = TestingSessionLocal()
+        yield db
+    finally:
+        db.close()
+
+    user = models.User(
+        username="test_user",
+        email="test_user@example.com",
+        hashed_password="password",
+        full_name="Test User",
+        disabled=False
+    )
+
+    return {"user": user, "token": "test_token"}
+
+
+app.dependency_overrides[get_db] = override_get_db
+
+
+@pytest.fixture(scope="function")
+def db():
+    session = TestingSessionLocal()
+    yield session
+    session.close()
+    Base.metadata.drop_all(bind=engine)
+    Base.metadata.create_all(bind=engine)
 
 
 def test_verify_password():
-    hashed_password = "$2b$12$vwK/hJWmk9hRuoiSyJ5wuOe2/qPeZ5JhgfE43mqzGp5n5jKEXJnX."
-    assert verify_password("password123", hashed_password)
+    password = "password"
+    hashed_password = get_password_hash(password)
+    assert verify_password(password, hashed_password)
     assert not verify_password("incorrectpassword", hashed_password)
 
 
 def test_get_password_hash():
     password = "password123"
     hashed_password = get_password_hash(password)
-    assert verify_password(password, hashed_password)
+    assert not password == hashed_password
 
 
-def test_get_user():
-    with get_db() as db:
-        user = create_user(db=db, user=UserCreate(username="testuser", email=create_random_email(), password="password"))
-        assert user.id is not None
+def test_get_user(db):
+    session = TestingSessionLocal()
+    user = create_user(db=session, user=schemas.UserCreate(
+        password="password", username="test_user_2", email="a@a.com", full_name="test user"))
+    assert user.id is not None
 
-        db_user = get_user(db=db, username=user.username)
-        assert db_user.username == user.username
+    db_user = get_user(db=session, username=user.username)
+    assert db_user.username == user.username
 
 
-def test_authenticate_user():
-    with get_db() as db:
-        user = create_user(db=db, user=UserCreate(username="testuser", email=create_random_email(), password="password"))
-        assert user.id is not None
+def test_authenticate_user(db):
+    session = TestingSessionLocal()
 
-        authenticated_user = authenticate_user(db=db, username=user.username, password="password")
-        assert authenticated_user.username == user.username
+    user = create_user(db=session, user=schemas.UserCreate(
+        username="test_user_3", email="testuser@test.com", password="password", full_name="test user"))
+    assert user.id is not None
 
-        authenticated_user = authenticate_user(db=db, username=user.username, password="incorrectpassword")
-        assert authenticated_user is False
+    authenticated_user = authenticate_user(
+        db=session, username=user.username, password="password")
+    assert authenticated_user.username == user.username
+
+    authenticated_user = authenticate_user(
+        db=session, username=user.username, password="incorrectpassword")
+    assert authenticated_user is False
 
 
 def test_create_access_token():
     data = {"sub": "testuser"}
     expires_delta = timedelta(minutes=15)
     access_token = create_access_token(data, expires_delta)
-    assert isinstance(access_token, bytes)
-
-
-def test_get_current_user():
-    with get_db() as db:
-        user = create_user(db=db, user=UserCreate(username="testuser", email=create_random_email(), password="password"))
-        assert user.id is not None
-
-        access_token = create_access_token(data={"sub": user.username})
-        headers = {"Authorization": f"Bearer {access_token.decode()}"}
-
-        # test that current user can be retrieved from token
-        response = app.dependency_overrides[get_db](db).get("/users/me", headers=headers)
-        assert response.status_code == 200
-        assert response.json()["username"] == user.username
-
-        # test that incorrect token leads to unauthorized error
-        headers = {"Authorization": "Bearer invalidtoken"}
-        response = app.dependency_overrides[get_db](db).get("/users/me", headers=headers)
-        assert response.status_code == 401
-
-
-def test_get_current_active_user():
-    with get_db() as db:
-        user = create_user(db=db, user=UserCreate(username="testuser", email=create_random_email(), password="password"))
-        assert user.id is not None
-
-        access_token = create_access_token(data={"sub": user.username})
-        headers = {"Authorization": f"Bearer {access_token.decode()}"}
-
-        # test that current active user can be retrieved from token
-        response = app.dependency_overrides[get_db](db).get("/users/me", headers=headers)
-        assert response.status_code == 200
-        assert response.json()["username"] == user.username
-
-        # test that inactive user leads to bad request error
-        db.execute("UPDATE users SET disabled=true WHERE username=:username", {"username":
+    assert "ey" in access_token
